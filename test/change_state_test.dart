9a003b41cc58eefd60c0c52880ea17265145a372
Eric Dias <ericdiasblu@gmail.com>
2025-07-23 09:55:31 -0300

    criando testes

---
+++ test/change_state_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gerenciamento_estado/controllers/change_state.dart';
+
+void main() {
+  group('Should test ChangeState', () {
+    test('Should increment Counter', () {
+      // Arrange
+      final _CounterChangeState changeState = _CounterChangeState();
+
+      // Act
+      changeState.increment();
+
+      // Assert
+      expect(changeState.counter, 1);
+    });
+
+    test('Should execute callback', () {
+      // Arrange
+      bool callbackExecuted = false;
+      final _CounterChangeState changeState = _CounterChangeState();
+
+      void callback() {
+        callbackExecuted = true;
+      }
+
+      // Act
+      changeState.addListener(callback);
+
+      changeState.increment();
+
+      // Assert
+      expect(changeState.counter, 1);
+      expect(callbackExecuted, true);
+    });
+  });
+}
+
+class _CounterChangeState extends ChangeState {
+  int counter = 0;
+
+  void increment() {
+    counter++;
+    notifyCallbacks();
+  }
+
+  void decrement() {
+    counter--;
+    notifyCallbacks();
+  }
+}
